09d5d217c1a1300d64a7b0457fb4c915652bfb11
Đỗ Thanh Sơn <son@os-MacBook-Air.local>
2026-03-11 18:22:21 +0700

    add funcs with no comparisons

--- sorts.cpp
+++ sorts.cpp
@@ -257,3 +257,246 @@ void shellSort(int a[], int n, unsigned long long& comp) {
     }
 }
 
+void no_comp_selectionSort(int a[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+
+        int min_idx = i;
+        for (int j = i + 1;j < n; j++) {
+            if (a[j] < a[min_idx]) {
+                min_idx = j;
+            }
+        }
+        swap(a[min_idx], a[i]);
+    }
+}
+
+void no_comp_insertionSort(int a[], int n){
+    for (int i = 1; i < n; i++){
+        int num = a[i];
+        int j = i-1;
+        while (j >= 0 && a[j] > num){
+            a[j + 1] = a[j];
+            j = j - 1;
+        }
+        a[j + 1] = num;
+    }
+}
+
+void no_comp_binaryinsertionSort(int a[], int n){
+    for (int i = 1;  i < n; ++i){
+        int key = a[i];
+        int first = 0, last = i - 1;
+        while (first <= last) {
+            int m = (first + last) / 2;
+            if (key < a[m]) last = m - 1;
+            else first = m + 1;
+        }
+        for (int j = i - 1; j >= first; --j)
+            a[j + 1] = a[j];
+        a[first] = key;
+    }
+}
+
+void no_comp_bubbleSort(int a[], int n){
+    for (int pass = 1; pass < n; ++pass){
+        bool checked = true;
+        for (int j = 0; j < n - pass; ++j){
+            if (a[j] > a[j + 1]){
+                swap(a[j], a[j + 1]);
+                checked = false;
+            }
+        }
+        if(checked){
+            return;
+        }
+    }
+}
+
+void no_comp_shakerSort(int a[], int n) {
+    int left = 1,right = n - 1,k = 0;
+    do {
+        k = 0;
+        for (int j = left; j <= right; ++j) {
+            if ( a[j - 1] > a[j]) {
+                swap(a[j - 1], a[j]);
+                k = j;
+            }
+        }
+        right = k - 1; 
+        for (int j = right; j >= left; --j) {
+            if ( a[j - 1] > a[j]) {
+                swap(a[j - 1], a[j]);
+                k = j;
+            }
+        }
+        left = k + 1; 
+    } while (left <= right && k > 0); 
+}
+
+void no_comp_heapify(int a[], int n, int i) {
+    int largest = i;       
+    int left = 2 * i + 1;  
+    int right = 2 * i + 2; 
+    if (left < n && ( a[left] > a[largest])) {
+        largest = left;
+    }
+    if (right < n && ( a[right] > a[largest])) {
+        largest = right;
+    }
+    if (largest != i) {
+        swap(a[i], a[largest]);
+        no_comp_heapify(a, n, largest);
+    }
+}
+
+void no_comp_heapSort(int a[], int n) { 
+    for (int i = n / 2 - 1; i >= 0; --i) {
+        no_comp_heapify(a, n, i);
+    }
+    for (int i = n - 1; i > 0; --i) {
+        swap(a[0], a[i]);
+        no_comp_heapify(a, i, 0);
+    }
+}
+
+void no_comp_merge(int a[], int left, int mid, int right) {
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+    int* L = new int[n1];
+    int* R = new int[n2];
+
+    for (int i = 0; i < n1; i++) L[i] = a[left + i];
+    for (int j = 0; j < n2; j++) R[j] = a[mid + 1 + j];
+
+    int i = 0, j = 0, k = left;
+    while (i < n1 && j < n2) {
+        if ( L[i] <= R[j]) {
+            a[k++] = L[i++];
+        } else {
+            a[k++] = R[j++];
+        }
+    }
+    while (i < n1) a[k++] = L[i++];
+    while (j < n2) a[k++] = R[j++];
+    delete[] L;
+    delete[] R;
+}
+
+void no_comp_mergeSortRec(int a[], int left, int right) {
+    if (left < right) {
+        int mid = left + (right - left) / 2;
+        no_comp_mergeSortRec(a, left, mid);
+        no_comp_mergeSortRec(a, mid + 1, right);
+        no_comp_merge(a, left, mid, right);
+    }
+}
+
+void no_comp_mergeSort(int a[], int n) {
+    no_comp_mergeSortRec(a, 0, n - 1);
+}
+
+void no_comp_quickSortRec(int a[], int left, int right) {
+    int i = left,j = right;
+    int pivot = a[left + (right - left) / 2];
+    while (i <= j) {
+        while ( a[i] < pivot) {
+            i++;
+        }
+        while ( a[j] > pivot) {
+            j--;
+        }
+        if ( i <= j) {
+            swap(a[i], a[j]);
+            i++;
+            j--;
+        }
+    }
+    if (left < j) {
+        no_comp_quickSortRec(a, left, j);
+    }
+    if (i < right) {
+        no_comp_quickSortRec(a, i, right);
+    }
+}
+
+void no_comp_quickSort(int a[], int n) {
+    if (n > 1) {
+        no_comp_quickSortRec(a, 0, n - 1);
+    }
+}
+
+void no_comp_countingSortForRadix(int a[], int n, int exp) {
+    int* output = new int[n];
+    int count[10] = {0};
+    for (int i = 0; i < n; i++) {
+        count[(a[i] / exp) % 10]++;
+    }
+    for (int i = 1;i < 10; i++) {
+        count[i] += count[i - 1];
+    }
+    for (int i = n - 1; i >= 0; i--) {
+        output[count[(a[i] / exp) % 10] - 1] = a[i];
+        count[(a[i] / exp) % 10]--;
+    }
+    for (int i = 0; i < n; i++) {
+        a[i] = output[i];
+    }
+    delete[] output;
+}
+
+void no_comp_radixSort(int a[], int n) {
+    if (n <= 1) return;
+    int max_val = a[0];
+    for (int i = 1; i < n; i++) {
+        if ( a[i] > max_val) {
+            max_val = a[i];
+        }
+    }
+    for (int exp = 1; max_val / exp > 0; exp *= 10) {
+        no_comp_countingSortForRadix(a, n, exp);
+    }
+}
+
+void no_comp_countingSort(int a[], int n) {
+    if (n <= 1) return;
+    int max_val = a[0];
+    for (int i = 1; i < n; i++) {
+        if ( a[i] > max_val) {
+            max_val = a[i];
+        }
+    }
+
+    int* count = new int[max_val + 1]{0};
+    int* output = new int[n];
+
+    for (int i = 0; i < n; i++) {
+        count[a[i]]++;
+    }
+    for (int i = 1; i <= max_val; i++) {
+        count[i] += count[i - 1];
+    }
+    for (int i = n - 1; i >= 0; i--) {
+        output[count[a[i]] - 1] = a[i];
+        count[a[i]]--;
+    }
+    for (int i = 0; i < n; i++) {
+        a[i] = output[i];
+    }
+
+    delete[] count;
+    delete[] output;
+}
+
+void no_comp_shellSort(int a[], int n) {
+    for (int gap = n / 2; gap > 0; gap /= 2) {
+        for (int i = gap; i < n; i += 1) {
+            int temp = a[i];
+            int j;
+            for (j = i; j >= gap && ( a[j - gap] > temp); j -= gap) {
+                a[j] = a[j - gap];
+            }
+            a[j] = temp;
+        }
+    }
+}
+
